acf87705885acb9102d7ba4b0b08af88759eb8fc
Jack Yeh <jack.yeh@gmail.com>
2015-12-01 14:16:33 -0700

    add namespaced routes specific for outside api access

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   mount MagicLamp::Genie, at: '/magic_lamp' if defined?(MagicLamp)
 
-  resources :ideas
+  namespace :api do
+    namespace :v1 do
+      resources :ideas
+    end
+  end
+
 end
 
 
