f2ecd4773b327bbbef21e71bbb15849dab88d793
Jack Yeh <jack.yeh@gmail.com>
2015-12-02 18:10:13 -0700

    create functionality: read all the ideas in the database

--- app/assets/javascripts/revenge_box.js
+++ app/assets/javascripts/revenge_box.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
   $.ajax({
     type: 'GET',
-    url: 'https://desolate-inlet-6608.herokuapp.com/ideas',
-    success: function(){
-      console.log('it worked')
+    url: 'https://desolate-inlet-6608.herokuapp.com/ideas.json',
+    success: function(posts){
+      $.each(posts, function(index, post){
+        $('#latest-posts').append(
+            "<div class='post' data-id='"
+            + post.id
+            + "'><h6>Published on "
+            + post.created_at
+            + "</h6><p>"
+            + post.title
+            + ":" + post.body
+            + "</p></div>"
+            )
+
+      });
     }
   });
 });
