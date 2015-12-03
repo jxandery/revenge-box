4344e5a5de64fe85eba317be8fa93fa68f6d5201
Jack Yeh <jack.yeh@gmail.com>
2015-12-02 20:59:48 -0700

    refactor: remove extraneous controller

--- app/controllers/api/v1/ideas_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::IdeasController < ApplicationController
-  respond_to :html, :json, :xml
-  #default api/v1/ideas with a json payload
-
-  def index
-    respond_with Idea.all
-  end
-end
