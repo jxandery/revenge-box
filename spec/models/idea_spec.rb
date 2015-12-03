4db138010dd0d2d1a868520bb44baef5e9ed7e2b
Jack Yeh <jack.yeh@gmail.com>
2015-12-02 23:57:45 -0700

    test setup: idea model

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with attributes" do
+    let(:idea) {Idea.create(title: "idea 1",
+                            body: "i hope i am not at this all night",
+                            quality: "swill")
+    }
+    it "is valid" do
+      expect(idea).to be_valid
+    end
+
+  end
 end
