d6bc1d0da9f7bfca28c939a39cc36bdb3e3e2901
Jack Yeh <jack.yeh@gmail.com>
2015-12-03 07:49:46 -0700

    add feature: edit forms hidden

--- app/assets/javascripts/revenge_box.js
+++ app/assets/javascripts/revenge_box.js
@@ -6,30 +6,32 @@ $(document).ready(function(){
 
 function editPost(){
   $('#edit-post').on('click', function(){
+    console.log('sliding');
+    //$('.edit').toggleklideDown();
     // slide down a form that is already in the html
-    var postDescription = $('#post-description').val();
-    var postBody = $('#post-body').val();
-    var postParams      = {
-      idea: {
-        title: postDescription,
-        body: postBody
-      }
-    }
+    //var postDescription = $('#post-description').val();
+    //var postBody = $('#post-body').val();
+    //var postParams      = {
+    //idea: {
+    //title: postDescription,
+    //body: postBody
+    //}
+    //}
 
-    $('#post-description').val('');
-    $('#post-body').val('');
+    //$('#post-description').val('');
+    //$('#post-body').val('');
 
-    $.ajax({
-      type: 'POST',
-      url: '/ideas.json',
-      data: postParams,
-      success: function(post){
-        renderPost(post);
-      },
-      error: function() {
-        alert('An idea title and body are required');
-      }
-    });
+    //$.ajax({
+    //type: 'POST',
+    //url: '/ideas.json',
+    //data: postParams,
+    //success: function(post){
+    //renderPost(post);
+    //},
+    //error: function() {
+    //alert('An idea title and body are required');
+    //}
+    //});
   });
 }
 
@@ -65,14 +67,14 @@ function renderPost(post) {
       + "<button id='thumbs-up' class='btn btn-default btn-xs'>Thumbs Up</button>"
       + "<button id='thumbs-down' class='btn btn-default btn-xs'>Thumbs down</button>"
       + "</p><button id='edit-post' class='btn btn-default btn-xs'>Edit</button>"
-      + "<button id='delete-post' class='btn btn-default btn-xs'>Delete</button></div>"
-      + "<div class='form-group'><div class='row'><div class='col-sm-4'><h6>Edit Title</h6>"
-      + "<input class='form-control' type='text' id='post-description'></div></div>"
-      + "<div class='row'><div class='col-sm-8'><h6>Edit Body</h6><input class='form-control' "
-      + " type='text' id='post-body'></div></div></div>"
-      + "<input class='btn btn-default pull-right' id='edit-post' type='button' name='submit'"
-      + " value='Update Idea'>"
+      + "<button id='delete-post' class='btn btn-default btn-xs'>Delete</button>"
+      //edit forms
+      + "<div class='form-group' id='hidden'><div class='row'><div class='col-sm-4'><h6>Edit Title</h6>"
+      + "<input class='form-control' type='text' id='post-description'></div></div><div class='row'><div class='col-sm-8'>"
+      + "<h6>Edit Body</h6><input class='form-control' type='text' id='post-body'></div></div><input "
+      + "class='btn btn-default pull-right' id='edit-post' type='button' name='submit' value='Update Idea'></div></div>"
       );
+  $('#hidden').hide();
 }
 
 function fetchPosts() {
