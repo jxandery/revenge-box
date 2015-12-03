1dc9a387d00b25259612148c5e261e4ec4365c0d
Jack Yeh <jack.yeh@gmail.com>
2015-12-03 00:21:18 -0700

    create/pass test: idea has a default quality property equal to swill

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Idea, type: :model do
   context "with attributes" do
     let(:idea) {Idea.create(title: "idea 1",
-                            body: "i hope i am not at this all night",
-                            quality: "swill")
+                            body: "i hope i am not at this all night")
     }
+
     it "is valid" do
       expect(idea).to be_valid
     end
@@ -20,6 +20,10 @@ RSpec.describe Idea, type: :model do
       idea.body = nil
 
       expect(idea).to_not be_valid
+    end
+
+    it "has a default quality of swill" do
+      expect(idea.quality).to eq('swill')
     end
   end
 end
