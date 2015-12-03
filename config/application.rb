0333a569a786377dfd20bc7b3d234f19b0ba1b32
Jack Yeh <jack.yeh@gmail.com>
2015-12-02 17:38:52 -0700

    fix typo

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RevengeBox
   class Application < Rails::Application
-    config.middleware.insert_before 0, "Rack:Cors" do
+    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
         resoource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
